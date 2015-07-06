617a43c6983120bfe8da1ce1a6a773aedf7ef217
Michał Powaga <michalpowaga13@gmail.com>
2015-07-06 23:27:39 +0100

    Add delete button

--- client/collections/collections.js
+++ client/collections/collections.js
@@ -1,3 +1,5 @@
+var adminDeleteModalCallback;
+
 Admin.dashboard.set('collectionWidgets', {
   template: 'mfAdminCollectionsWidgets'
 });
@@ -7,4 +9,33 @@ Template.mfAdminCollectionsWidgets.onCreated(function () {
   _.each(Admin.collections.get(), function (collection) {
     self.subscribe(collection.countPubName)
   });
+});
+
+Template.mfAdminCollectionsView.events({
+  // This callback should be attached to mfAdminCollectionsDeleteBtn template
+  // but for some reason templates rendered with Blaze.renderWithData are not
+  // firing event callbacks
+  'click .js-delete-doc': function (e, t) {
+    var collection = this.table.collection;
+    var _id = $(e.currentTarget).attr('data-id');
+
+    adminDeleteModalCallback = function () {
+      collection.remove(_id);
+    };
+
+    $('#admin-delete-modal').modal('show');
+  }
+});
+
+Template.mfAdminCollectionsDeleteModal.events({
+  'click .js-delete': function (e, t) {
+    if (typeof adminDeleteModalCallback === 'function') {
+      adminDeleteModalCallback();
+      t.$('#admin-delete-modal').modal('hide');
+    }
+  },
+
+  'hidden.bs.modal #admin-delete-modal': function (e, t) {
+    adminDeleteModalCallback = null;
+  }
 });
