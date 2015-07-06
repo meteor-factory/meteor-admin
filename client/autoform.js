45e5f01e6cb2ca924650e54ae8ab894676ab3c1d
Michał Powaga <michalpowaga13@gmail.com>
2015-07-06 23:40:29 +0100

    Navigate to collection view on insert success

--- client/autoform.js
+++ client/autoform.js
@@ -1,4 +1,10 @@
 AutoForm.hooks({
+  mfAdmin_insert: {
+    onSuccess: function (formType, collection) {
+      Admin.go(this.template.data._successPath);
+    }
+  },
+
   mfAdmin_update: {
     onSuccess: function (formType, collection) {
       Admin.go(this.template.data._successPath);
